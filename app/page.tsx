848f8c0c06fb010155e0482839eb8cc150d35f19
kiril <kirill_klim@list.ru>
2026-02-16 17:51:33 +0600

    Implement coin overview and trending coin

--- app/page.tsx
+++ app/page.tsx
@@ -1,122 +1,22 @@
-import DataTable from "@/components/DataTable";
-import Header from "@/components/Header";
-import { cn } from "@/lib/utils";
-import { TrendingDown, TrendingUp } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/Fallback";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
 
-const columns: DataTableColumn<TrendingCoin>[] = [
-  {
-    header: "Name",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-
-      return (
-        <Link href={`/coins/${item.id}`}>
-          <Image src={item.large} alt={item.name} width={36} height={36} />
-          <p>{item.name}</p>
-        </Link>
-      );
-    },
-  },
-  {
-    header: "24h Change",
-    cellClassName: "name-cell",
-    cell: (coin) => {
-      const item = coin.item;
-      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
-
-      return (
-        <div
-          className={cn(
-            "price-change",
-            isTrendingUp ? "text-green-500" : "text-red-500",
-          )}
-        >
-          <p>
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
-            )}
-            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-          </p>
-        </div>
-      );
-    },
-  },
-  {
-    header: "Price",
-    cellClassName: "price-cell",
-    cell: (coin) => `$${coin.item.data.price.toLocaleString()}`,
-  },
-];
-
-const dummyTrendingCoins: TrendingCoin[] = [
-  {
-    item: {
-      id: "bitcoin",
-      name: "Bitcoin",
-      symbol: "BTC",
-      market_cap_rank: 1,
-      thumb: "/logo.svg",
-      large: "/logo.svg",
-      data: {
-        price: 89113.0,
-        price_change_percentage_24h: {
-          usd: 2.5,
-        },
-      },
-    },
-  },
-  {
-    item: {
-      id: "ethereum",
-      name: "Ethereum",
-      symbol: "ETH",
-      market_cap_rank: 2,
-      thumb: "/logo.svg",
-      large: "/logo.svg",
-      data: {
-        price: 2500.0,
-        price_change_percentage_24h: {
-          usd: -1.2,
-        },
-      },
-    },
-  },
-];
-
-const Page = () => {
+const Page = async () => {
   return (
     <main className="main-container">
       <section className="home-grid">
-        <div id="coin-overview">
-          <div className="header pt-2">
-            <Image
-              src="https://assets.coingecko.com/coins/images/1/standard/bitcoin.png?1696501400"
-              alt="Bitcoin"
-              width={56}
-              height={56}
-            />
-            <div className="info">
-              <p>BitCoin / BTC</p>
-              <h1>$89,113.00</h1>
-            </div>
-          </div>
-        </div>
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
 
-        <p>Trending coins</p>
-        <div className="trending-coins">
-          <DataTable
-            data={dummyTrendingCoins}
-            columns={columns}
-            rowKey={(coin) => coin.item.id}
-            tableClassName="trending-coins-table"
-          />
-        </div>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
       </section>
 
       <section className="w-full mt-7 space-y-4">
